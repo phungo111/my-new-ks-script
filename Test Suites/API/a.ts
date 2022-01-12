0eef383f31e56d364fbf1ce7736f9feb087f1988
phuhuyngo <phuhuyngo@C02DV9Y1ML7H.local>
2022-01-12 15:49:31 +0700

    replace u2 with ú

--- Test Suites/API/a.ts
+++ Test Suites/API/a.ts
@@ -3,7 +3,7 @@
    <description></description>
    <name>a</name>
    <tag></tag>
-   <executionMode>PARALLEL</executionMode>
+   <executionMode>SEQUENTIAL</executionMode>
    <maxConcurrentInstances>8</maxConcurrentInstances>
    <testSuiteRunConfigurations>
       <TestSuiteRunConfiguration>
